89a750247957a929cfabe86e09b05009dbe3b179
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-09-18 09:48:18 +0300

    + favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,29 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Chatiko',
   description: 'Welcome to Chatiko realtime chat app',
+  icons: {
+    icon: [
+      {
+        url: '/favicon/favicon-32x32.png',
+        type: 'image/png',
+        sizes: '32x32',
+      },
+      {
+        url: '/favicon/favicon-16x16.png',
+        type: 'image/png',
+        sizes: '16x16',
+      },
+    ],
+    apple: {
+      url: '/favicon/apple-touch-icon.png',
+      sizes: '180x180'
+    },
+  },
+  manifest: '/favicon/site.webmanifest',
+  themeColor: '#fff',
 }
 
+// TODO: install tailwind prittier plugin
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
